f22d08c312a2c5053b459dc72e9dd9cbccc2ad03
04dan <04dan@LAPTOP-35D3RKBD>
2023-02-25 11:57:49 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1NegativoCeroPositivo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ejercicio1NegativoCeroPositivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ejercicio 1: número entero, cero o negativo");
+        inicio:
+            Console.WriteLine("Escriba un número entero: ");
+            bool convertido = int.TryParse(Console.ReadLine(), out int n1);
+
+            if (convertido == true)
+            {
+                goto convertir;
+            }
+            else
+            {
+                goto inicio;
+            }
+
+        convertir:  int n11 = Convert.ToInt32(n1.ToString());
+
+
+            if (n11 == 0)
+            {
+                Console.WriteLine("RESULTADO>> CERO");
+            }else if (n11 < 0)
+            {
+                Console.WriteLine("RESULTADO>> NEGATIVO");
+            }
+            else if (n11 > 0)
+            {
+                Console.WriteLine("RESULTADO>> POSITIVO");
+            }
+
+        }
+    }
+}
+ 
